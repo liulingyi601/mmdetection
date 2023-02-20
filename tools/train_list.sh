25e7999205350ee5a99b18943e595630b1c478dc
601 <liu601715@163.com>
2023-02-20 11:20:02 +0800

    修改多卡自动识别

--- tools/train_list.sh
+++ tools/train_list.sh
@@ -8,7 +8,7 @@ echo $name
 echo "******************************************"
 # CUDA_VISIBLE_DEVICES=$GPUS PORT=$PORT python ./tools/print_config.py configs/$name.py
 # CUDA_VISIBLE_DEVICES=$GPUS PORT=$PORT ./tools/dist_train.sh configs_sar/$name.py 2 
-CUDA_VISIBLE_DEVICES=$GPUS PORT=$PORT ./tools/dist_train.sh $configs/$name.py 2 
+CUDA_VISIBLE_DEVICES=$GPUS PORT=$PORT ./tools/dist_train.sh $configs/$name.py $[(${#GPUS}+1)/2] 
 
 sleep 20
 done
